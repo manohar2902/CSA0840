ef681ad430fbddebbb01adddaa32b5d8d2bd884b
manohar2902 <113917642+manohar2902@users.noreply.github.com>
2022-09-24 08:43:29 +0530

    Update Robert problem.py

--- Robert problem.py
+++ Robert problem.py
@@ -1,16 +1,8 @@
-testSize = 2
-nArr = []
-for i in range(1, testSize + 1):
-    nArr.append(int(input()))
-
-for n in nArr:
-    if n >= 1 and n <= (10 ** 9):
-        prod = 1
-        sum = 0
-        for i in range(1, n + 1):
-            prod = prod * i
-            sum = sum + i
-        if prod % sum == 0:
-            print("YEAH")
-        else:
-            print("NAH")
+n1=int(input("enter:"))
+n2=int(input("enter:"))
+n=n1*n2
+s=n1+n2
+if(n%s==0):
+   print("YEAH")
+else:
+    print("NAH")
